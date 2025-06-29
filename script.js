f57a2445c73f2e0c1b06fb583929d91d3c16bcc3
ha0177 <214249506+Ha0177@users.noreply.github.com>
2025-06-29 11:05:31 +0300

    Add grid size input slider and dynamic square generation

--- script.js
+++ script.js
@@ -10,5 +10,16 @@ gridContainer.addEventListener("mouseover", (e) => {
     if (e.target.matches("div.square")) {
         e.target.style.backgroundColor = "black";
     }
+});
 
-})
+const slider = document.querySelector("input#gridSize")
+const gridSize = slider.value;
+const squareSize = 800 / gridSize;
+
+for (let index = 0; index < gridSize * gridSize; index++) {
+    const squareDiv = document.createElement("div");
+    squareDiv.classList.add("square");
+    squareDiv.style.width = squareSize + "px";
+    squareDiv.style.height = squareSize + "px";
+    gridContainer.appendChild(squareDiv);
+}

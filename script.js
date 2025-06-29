d290c1ba342b7efdafbfb4554c267499552161a8
ha0177 <214249506+Ha0177@users.noreply.github.com>
2025-06-29 11:18:00 +0300

    Refactor grid creation logic to use a function and update grid size dynamically with slider input

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const gridContainer = document.querySelector("div.grid-container");
 
-for (let index = 0; index < 256; index++) {
-    const squareDiv = document.createElement("div");
-    squareDiv.classList.add("square");
-    gridContainer.appendChild(squareDiv);
-}
+// for (let index = 0; index < 256; index++) {
+//     const squareDiv = document.createElement("div");
+//     squareDiv.classList.add("square");
+//     gridContainer.appendChild(squareDiv);
+// }
 
 gridContainer.addEventListener("mouseover", (e) => {
     if (e.target.matches("div.square")) {
@@ -13,13 +13,24 @@ gridContainer.addEventListener("mouseover", (e) => {
 });
 
 const slider = document.querySelector("input#gridSize")
-const gridSize = slider.value;
-const squareSize = 800 / gridSize;
 
-for (let index = 0; index < gridSize * gridSize; index++) {
-    const squareDiv = document.createElement("div");
-    squareDiv.classList.add("square");
-    squareDiv.style.width = squareSize + "px";
-    squareDiv.style.height = squareSize + "px";
-    gridContainer.appendChild(squareDiv);
+function createGrid() {
+    gridContainer.innerHTML = "";
+    const gridSize = slider.value;
+    const squareSize = 800 / gridSize;
+
+    const gridValue = document.querySelector("span#gridValue");
+    gridValue.innerText = gridSize + " x " + gridSize;
+    
+    for (let index = 0; index < gridSize * gridSize; index++) {
+        const squareDiv = document.createElement("div");
+        squareDiv.classList.add("square");
+        squareDiv.style.width = squareSize + "px";
+        squareDiv.style.height = squareSize + "px";
+        gridContainer.appendChild(squareDiv);
+    }
 }
+
+slider.addEventListener("input", createGrid);
+
+createGrid();

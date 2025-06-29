1dd9727f91e73a306f3fcad007563ea41e7e79a6
ha0177 <214249506+Ha0177@users.noreply.github.com>
2025-06-29 12:02:04 +0300

    Add color mode buttons for default and rainbow drawing options

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const gridContainer = document.querySelector("div.grid-container");
+let currentColor = "black";
 
-let isDrawing = false;
+function gridDrawing() {
+    let isDrawing = false;
 
-gridContainer.addEventListener("mousedown", () =>{
-    isDrawing = true;
-});
-gridContainer.addEventListener("mouseup", () =>{
-    isDrawing = false;
-});
+    gridContainer.addEventListener("mousedown", () =>{
+        isDrawing = true;
+    });
+    gridContainer.addEventListener("mouseup", () =>{
+        isDrawing = false;
+    });
 
-gridContainer.addEventListener("mouseover", (e) => {
-    if (e.target.matches("div.square") && isDrawing) {
-        e.target.style.backgroundColor = "black";
-    }
-});
+    gridContainer.addEventListener("mouseover", (e) => {
+        if (e.target.matches("div.square") && isDrawing) {
+            if (currentColor === "rainbow") {
+                e.target.style.backgroundColor = rainbowColor();
+            } else {
+                e.target.style.backgroundColor = currentColor;
+            }
+        }
+    });
+}
+gridDrawing();
 
 const slider = document.querySelector("input#gridSize")
 
@@ -44,3 +52,20 @@ function resetBtn() {
     
 }
 resetBtn();
+
+const defaultBtn = document.querySelector("button.default-btn");
+defaultBtn.addEventListener("click", () => {
+    currentColor = "black";
+});
+
+function rainbowColor() {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+    return `rgb(${r}, ${g}, ${b})`;
+}
+const rainbowBtn = document.querySelector("button.rainbow-btn");
+rainbowBtn.addEventListener("click", () => {
+    currentColor = "rainbow";
+});
+
